0b10ba7b59a4224917f9e71ea34df53432f9acd5
ciprianbpc <155395856+ciprianbpc@users.noreply.github.com>
2025-02-18 05:40:16 +0200

    Update addGoal.js
    second commit

---
+++ controllers/user/addGoal.js
@@ -0,0 +1,28 @@
+const { Users, Food, Water } = require('../../models');
+const { nutrientsTotalPerDay, asyncHandler } = require('../../utils');
+
+const addGoal = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+    const { goal } = req.body;
+    
+    const user = req.user;
+    const owner = String(_id);
+    
+    const beginDate = new Date();
+    const endDate = new Date();
+    beginDate.setHours(0, 0, 0, 0);
+    endDate.setHours(23, 59, 59, 999);
+
+    const updatedUser = await Users.findByIdAndUpdate(_id, { goal }, { returnDocument: 'after' });
+
+    const foodData = await Food.findOne({ owner, date: { $gte: beginDate, $lte: endDate } });
+    const waterData = await Water.findOne({ owner, date: { $gte: beginDate, $lte: endDate } });
+    const total = nutrientsTotalPerDay(user, waterData, foodData);
+
+    res.status(200).json({
+        goal: updatedUser.goal,
+        total,
+    });
+});
+
+module.exports = { addGoal };
