e92699147d7df940d59d8b3cb1643e5f4c10e9af
ciprianbpc <155395856+ciprianbpc@users.noreply.github.com>
2025-02-18 06:29:40 +0200

    5
    5

---
+++ routes/authRoutes.js
@@ -0,0 +1,58 @@
+
+const express = require('express');
+const authRoutes = express.Router();
+
+const schema = require('../../models');
+const middleware = require('../../middleware');
+const controller = require('../../controllers/auth');
+
+authRoutes.get(
+    '/current',
+    middleware.authentication,
+    controller.currentUser
+);
+
+authRoutes.post(
+    '/register',
+    middleware.validateRequest(schema.validateRegister),
+    controller.registerUser
+);
+
+authRoutes.post(
+    '/login',
+    middleware.validateRequest(schema.validateLogin),
+    controller.loginUser
+);
+
+authRoutes.post(
+    '/logout',
+    middleware.authentication,
+    controller.logoutUser
+);
+
+authRoutes.post(
+    '/restore',
+    middleware.validateRequest(schema.validateEmail),
+    controller.restorePassword
+);
+
+authRoutes.patch(
+    '/avatar',
+    middleware.authentication,
+    middleware.downloadFromClient.single('file'),
+    controller.updateAvatar
+);
+
+authRoutes.post(
+    '/checking-registered',
+    middleware.validateRequest(schema.emailSchema),
+    controller.checkingRegistered
+);
+authRoutes.patch(
+    '/settings',
+    middleware.authentication,
+    middleware.validateRequest(schema.validateSettings),
+    controller.userSettings
+);
+
+module.exports = { authRoutes };
---
+++ routes/userRoutes.js
@@ -0,0 +1,89 @@
+
+const express = require('express');
+const userRoutes = express.Router();
+
+const schema = require('../../models');
+const middleware = require('../../middleware');
+const controller = require('../../controllers/user');
+
+userRoutes.patch(
+    '/goal',
+    middleware.authentication,
+    middleware.validateRequest(schema.validateGoal),
+    controller.addGoal
+);
+
+userRoutes.put(
+    '/food-intake/:id',
+    middleware.authentication,
+    middleware.validateID,
+    middleware.validateRequest(schema.validateDish),
+    controller.updatedFoodIntake
+);
+userRoutes.post(
+    '/water-intake',
+    middleware.authentication,
+    middleware.validateRequest(schema.waterSchema),
+    controller.waterIntake
+);
+userRoutes.get(
+    '/recommended-food',
+    middleware.authentication,
+    controller.getRecommendedFood
+);
+
+userRoutes.put(
+    '/weight',
+    middleware.authentication,
+    middleware.validateRequest(schema.weightSchema),
+    controller.weight
+);
+
+userRoutes.get(
+    '/statistics',
+    middleware.authentication,
+    middleware.validateRequest(schema.validateDate),
+    controller.getStatistics
+);
+
+userRoutes.get(
+    '/graph',
+    middleware.authentication,
+    controller.getGraph
+);
+
+userRoutes.get(
+    '/graph-v2',
+    middleware.authentication,
+    controller.getGraphV2
+);
+
+userRoutes.get(
+    '/food-intake',
+    middleware.authentication,
+    middleware.validateRequest(schema.foodSchema),
+    controller.getFoodIntake
+);
+
+userRoutes.post(
+    '/food-intake',
+    middleware.authentication,
+    middleware.validateRequest(schema.foodSchema),
+    controller.foodIntake
+);
+
+userRoutes.post(
+    '/food-intake-v2',
+    middleware.authentication,
+    middleware.validateRequest(schema.foodSchema),
+    controller.foodIntakeV2
+);
+
+userRoutes.post(
+    '/generate-demo',
+    middleware.authentication,
+    middleware.validateRequest(schema.validateDemo),
+    controller.generateDemo
+);
+
+module.exports = { userRoutes };
